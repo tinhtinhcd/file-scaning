c5e3805938260e7b7d9f07518f44a866bbbbdcac
vn51d28 <van.tinh.ly@samsclub.com>
2021-03-31 20:16:00 -0500

    create project

---
+++ src/test/java/demo/file/scanner/file/scanner/FileScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.file.scanner.file.scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
